5172d9b6c4630fcf012da3a5832d2f9542304f50
André Carlos <andre.carlos1993@hotmail.com>
2018-10-21 21:11:25 -0300

    Subtotal e total do Pedido

--- src/main/java/com/andrecarlos/cursomc/domain/ItemPedido.java
+++ src/main/java/com/andrecarlos/cursomc/domain/ItemPedido.java
@@ -30,6 +30,10 @@ public class ItemPedido implements Serializable {
 		this.preco = preco;
 	}
 
+	public double getSubTotal() {
+		return (preco - desconto) * quantidade;
+	}
+	
 	// Tudo que começa com get ele entende que tem que ser serializar
 	@JsonIgnore // Para evitar referencia ciclica, não irá serializar a lista de pedidos
 	public Pedido getPedido() {

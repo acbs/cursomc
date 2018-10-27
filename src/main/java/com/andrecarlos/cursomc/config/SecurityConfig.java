88676f961f13a1a0386ee335eae5998a1d796b92
André Carlos <andre.carlos1993@hotmail.com>
2018-10-27 00:07:06 -0300

    Configuração inicial do Spring Security

---
+++ src/main/java/com/andrecarlos/cursomc/config/SecurityConfig.java
@@ -0,0 +1,63 @@
+package com.andrecarlos.cursomc.config;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Autowired
+	private Environment env;
+	
+	private static final String[] PUBLIC_MATCHERS = {
+			"/h2-console/**"
+	};
+	
+	private static final String[] PUBLIC_MATCHERS_GET = {
+			"/produtos/**",
+			"/categorias/**",
+	};
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		
+		// Se estiver no perfil de test, será liberado o acesso ao bd H2
+		if (Arrays.asList(env.getActiveProfiles()).contains("test")) {
+            http.headers().frameOptions().disable();
+        }
+		
+		http.cors() // Ativando o Bean corsConfigurationSource (Para mais informações, material de apoio 06-autenticacao)
+			.and().csrf().disable(); // Como não armazenamos sessão, desabilitamos o ataque csrf (Baseado no armazenamento de sessão) 
+		
+		// Permitindo todos os caminhos que estiver no PUBLIC_MATCHERS
+		// anyRequest().authenticated() Para todo o resto, exige autenticação
+		http.authorizeRequests()
+			.antMatchers(HttpMethod.GET, PUBLIC_MATCHERS_GET).permitAll() // Permitindo apenas para o metodo GET	
+			.antMatchers(PUBLIC_MATCHERS).permitAll()
+			.anyRequest().authenticated();
+		
+		// Assegurando que o backend não criará sessão de usuário
+		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+	}
+
+	// Permitindo acesso aos endpoints com multiplas fontes com as configurações básicas
+	@Bean
+	CorsConfigurationSource corsConfigurationSource() {
+		final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", new CorsConfiguration().applyPermitDefaultValues());
+		return source;
+	}
+}
--- src/main/java/com/andrecarlos/cursomc/services/PedidoService.java
+++ src/main/java/com/andrecarlos/cursomc/services/PedidoService.java
@@ -75,7 +75,7 @@ public class PedidoService {
 		// Salvando o ItemPedido
 		itemPedidoRepository.saveAll(obj.getItens());
 		// Para enviar email de confirmação
-		emailService.sendOrderConfirmationHtmlEmail(obj);
+//		emailService.sendOrderConfirmationHtmlEmail(obj);
 		return obj;
 	}
 }

67489ac5a83f653c77a4c4537302aebebebc327c
André Carlos <andre.carlos1993@hotmail.com>
2018-10-28 22:52:20 -0300

    Salvando primeiro arquivo no S3

--- src/main/java/com/andrecarlos/cursomc/CursomcApplication.java
+++ src/main/java/com/andrecarlos/cursomc/CursomcApplication.java
@@ -1,14 +1,18 @@
 package com.andrecarlos.cursomc;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.andrecarlos.cursomc.services.S3Service;
 
 // CommandLineRunner(run) - Utilizado para executar alguma ação quando aplicação for iniciada
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner {
 
-
+	@Autowired
+	private S3Service s3Service;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -16,5 +20,6 @@ public class CursomcApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
+		s3Service.uploadFile("D:\\Faculdade\\Curso Spring Boot Udemy\\pcx.jpg");
 	}
 }
